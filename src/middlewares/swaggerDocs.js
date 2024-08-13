dc6fc4f9f070dcf6c2f1d51347189a03592a200b
Anna Samtash <sansara6900@gmail.com>
2024-08-13 18:02:31 +0300

    small fix

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -6,7 +6,6 @@ import { SWAGGER_PATH } from '../constants/index.js';
 export const swaggerDocs = (req, res, next) => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    console.log(swaggerDoc);
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch {
     next(createHttpError(500, "Can't load swagger docs"));
